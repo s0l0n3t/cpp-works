bbe12b493cdbd9d59b611aae2085b669800e12d8
s0l0n3t <furkantokgz@gmail.com>
2016-06-07 15:18:09 +0300

    you must put some spaces

---
+++ zodiac.cpp
@@ -0,0 +1,265 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+int date, month;
+int press;
+
+
+
+start:
+system ( "cls");
+cout << "\ t \ t \ n \ n"
+"It displays the features of your horoscope. \ N \ n"
+"When were you born? \ N \ n \ t \ colt:";
+cin >> month;
+
+cout << "\ n \ n \ t \ t" << "Day";
+
+
+cin >> date;
+
+system ( "cls");
+if (month == 3 && date> = 21 || == 4 && month date <= 20) {
+
+cout << "\ t \ tkoç burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: Men";
+cout << endl;
+cout << "Bastion of your planet: Mars";
+cout << endl;
+cout << "Group: fire";
+cout << endl;
+cout << "capabilities of your horoscope: Vitality and movement";
+cout << endl;
+cout << "Your goal: Success and victory";
+cout << endl;
+cout << "you have eaten your habit: impatience";
+cout << endl;
+}
+else if (month == 4 && date> = 21 || == 5 && month date <= 21) {
+
+cout << "\ t \ tbog burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: female";
+cout << endl;
+cout << "Bastion of your planet: Venus";
+cout << endl;
+cout << "Group: Soil negative";
+cout << endl;
+cout << "Bastion's abilities: to be warm-blooded, and he wanted to know";
+cout << endl;
+cout << "Your goal: goods, property, luxury and having great wealth";
+cout << endl;
+cout << "you have eaten your temperament: Susceptibility offended to luxury longing and selfishness";
+cout << endl;
+}
+else if (month == 5 && date> = 22 || == 6 month date && <= 22) {
+
+cout << "\ t \ compact Are burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: Men";
+cout << endl;
+cout << "Bastion of your planet: Mercury";
+cout << endl;
+cout << "Group: Air-positive";
+cout << endl;
+cout << "Bastion's capabilities: superior intelligence";
+cout << endl;
+cout << "Your goal: Success and victory";
+cout << endl;
+cout << "you have eaten your temperament: Very talk";
+cout << endl;
+}
+else if (month == 6 && date> = 23 && date 7 == || month <= 22) {
+cout << "\ t \ tyengeç burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: female";
+cout << endl;
+cout << "Bastion of your planet: the Moon";
+cout << endl;
+cout << "Group: Water, negative";
+cout << endl;
+cout << "capabilities of your horoscope: be patient";
+cout << endl;
+cout << "Your goal: Always to rise";
+cout << endl;
+cout << "you have eaten your temperament: Inattention";
+cout << endl;
+}
+else if (month == 7 && date> = 23 || == 8 && month date <= 22) {
+cout << "\ t \ draft of your burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: Men";
+cout << endl;
+cout << "Bastion of your planet: the Sun";
+cout << endl;
+cout << "Group: Fire-positive";
+cout << endl;
+cout << "capabilities of your horoscope: Plans to establish";
+cout << endl;
+cout << "Your goal: to achieve a lot";
+cout << endl;
+cout << "you have eaten your temperament: like yourself";
+cout << endl;
+}
+else if (month == 8 && date> = 23 month || == 9 && date <= 22) {
+cout << "\ t \ tbaşak burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: female";
+cout << endl;
+cout << "Bastion of your planet: Mars";
+cout << endl;
+cout << "Group: Soil negative";
+cout << endl;
+cout << "Bastion's capabilities: a disciplined and methodical life";
+cout << endl;
+cout << "Your goal: Success is always the way to rise";
+cout << endl;
+cout << "you have eaten your temperament: Shyness and discouragement";
+cout << endl;
+}
+else if (month == 9 && date> = 23 month || == 10 && date <= 22) {
+cout << "\ t \ tteraz your burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: Men";
+cout << endl;
+cout << "Bastion of your planet: Venus";
+cout << endl;
+cout << "Group: Air negative";
+cout << endl;
+cout << "Bastion's capabilities: Balanced and be sober";
+cout << endl;
+cout << "Your goal: To become a regular life";
+cout << endl;
+cout << "you have eaten your habit: to enjoy showing off";
+cout << endl;
+}
+else if (month == 10 && date> = 11 && date == 23 || month <= 21) {
+cout << "\ t \ takrep burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: female";
+cout << endl;
+cout << "Bastion of your planet: Mars and Pluto";
+cout << endl;
+cout << "Group: Water, negative";
+cout << endl;
+cout << "capabilities of your horoscope: Intuition";
+cout << endl;
+cout << "Your goal: lifelong wealth";
+cout << endl;
+cout << "you have eaten your habit: to pity, to neglect";
+cout << endl;
+}
+else if (month == 11 && date> = 12 && date == 22 || month <= 21) {
+cout << "\ t \ tyay burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: Men";
+cout << endl;
+cout << "Bastion of your planet: Jupiter";
+cout << endl;
+cout << "Group: Fire-positive";
+cout << endl;
+cout << "Bastion's capabilities: self-confidence";
+cout << endl;
+cout << "Your goal: Very sightseeing see, so see to achieve";
+cout << endl;
+cout << "you have eaten your habit: to belittle others";
+cout << endl;
+}
+else if (month == 12 && date> = 22 && || == 1 month date <= 21) {
+cout << "\ t \ toğlak burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: female";
+cout << endl;
+cout << "Bastion of your planet: Saturn";
+cout << endl;
+cout << "Group: Soil negative";
+cout << endl;
+cout << "capabilities of your horoscope: Decision and resistance";
+cout << endl;
+cout << "Your goal: stay mainly High Reach";
+cout << endl;
+cout << "you have eaten your temperament: More curiosity";
+cout << endl;
+}
+else if (month == 1 && date> = 22 && date == 2 || month <= 19) {
+cout << "\ t \ tkov burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: Men";
+cout << endl;
+cout << "Bastion your planet Uranus";
+cout << endl;
+cout << "Group: Air-positive";
+cout << endl;
+cout << "Bastion's capabilities: common sense";
+cout << endl;
+cout << "Your goal: to be promoted, to help others";
+cout << endl;
+cout << "you have eaten your temperament: negligence and not worth the money";
+cout << endl;
+}
+else if (month == 2 && date> = 20 && || == 3 month date <= 20) {
+cout << "\ t \ tbalık burcus";
+cout << endl;
+cout << "\ n \ n \ t \ t \ tözellik are:";
+cout << endl;
+cout << "Bastion of your gender: female";
+cout << endl;
+cout << "Bastion of your planet: Neptune";
+cout << endl;
+cout << "Group: Water, negative";
+cout << endl;
+cout << "Bastion of your abilities: attention to others, compassion";
+cout << endl;
+cout << "Your goal: achieve wealth and happiness";
+cout << endl;
+cout << "you have eaten your habit: to underestimate your own value";
+cout << endl;
+}
+else {
+
+cout << "Incorrect input please try again ...";
+system ( "PAUSE");
+system ( "cls");
+goto start;
+}
+cout << "to try again, 1"
+"For your keys or exit 2 \ n";
+cin >> press;
+if (press == 1) {
+goto start;
+}
+if (press == 2) {
+cout << "\ n";
+}
+return 0;
+cout << endl;
+
+
+
+}
+
